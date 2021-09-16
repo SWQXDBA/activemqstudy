3822f7016192cdc028906fe630db5dadaeeae296
SWQXDBA <983110853@qq.com>
2021-09-16 12:42:02 +0800

    1

--- src/test/java/com/example/activemqstudy/ActivemqstudyApplicationTests.java
+++ src/test/java/com/example/activemqstudy/ActivemqstudyApplicationTests.java
@@ -225,4 +225,115 @@ class ActivemqstudyApplicationTests {
     }
 
 
+    @Test
+    void contextLoadsConsumerTopic订阅发布() {//生产者
+        //使用队列
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(url);
+        try {
+            Connection connection = factory.createConnection();//创建连接
+
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+
+            //创建Destination
+            Topic topic = session.createTopic(topicName);
+
+
+            //创建生产者
+            MessageProducer producer = session.createProducer(topic);
+            //设置持久化模式
+            producer.setDeliveryMode(DeliveryMode.PERSISTENT);
+
+            connection.start();
+            for (int i = 0; i < 3; i++) {
+                TextMessage topicMessage = session.createTextMessage("topicTextMessage" + i);
+                MapMessage mapMessage = session.createMapMessage();
+                mapMessage.setString("per", "name" + i);
+                if (i == 2) {
+                    mapMessage.setStringProperty("level", "vip");
+                }
+                producer.send(mapMessage);
+                producer.send(topicMessage);
+            }
+
+            //注册监听器
+
+            producer.close();
+            session.close();
+            connection.close();
+        } catch (JMSException jmsException) {
+            jmsException.printStackTrace();
+        }
+
+
+    }
+
+    @Test
+    void contextLoadsConsumerTopic2订阅发布() {//topic的消费者
+        //使用队列
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(url);
+        try {
+            Connection connection = factory.createConnection();//创建连接
+            connection.setClientID("s1");
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+
+            //创建Destination
+            Topic topic = session.createTopic(topicName);
+
+            //创建消费者
+            TopicSubscriber consumer = session.createDurableSubscriber(topic, "备注");
+
+
+            connection.start();
+            //注册监听器
+            consumer.setMessageListener(message -> {
+
+                if (null == message)
+                    return;
+                if (message instanceof TextMessage) {
+                    TextMessage textMessage = (TextMessage) message;
+                    try {
+                        System.out.println(textMessage.getText());
+                    } catch (JMSException e) {
+                        e.printStackTrace();
+                    }
+                } else if (message instanceof MapMessage) {
+                    MapMessage mapMessage = (MapMessage) message;
+                    try {
+                        String person = mapMessage.getString("per");
+                        System.out.println(person);
+
+                        if (mapMessage.propertyExists("level")) {
+
+                            System.out.println(mapMessage.getStringProperty("level"));
+                        }
+                    } catch (JMSException e) {
+                        e.printStackTrace();
+                    }
+                } else if (message instanceof ObjectMessage) {
+                    ObjectMessage mapMessage = (ObjectMessage) message;
+                    try {
+                        Object o = mapMessage.getObject();
+                        System.out.println(o);
+                    } catch (JMSException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+            });
+
+
+            System.in.read();
+            consumer.close();
+            session.close();
+            connection.close();
+        } catch (JMSException | IOException jmsException) {
+            jmsException.printStackTrace();
+        }
+
+
+    }
+
 }
